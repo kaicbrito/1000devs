d9d23e9b54d89070db93beeb16d17b91b49e323c
kaicbrito <kaicbritor@gmail.com>
2023-08-21 14:23:13 -0300

    update

---
+++ exercicio-if-else/app.js
@@ -0,0 +1,12 @@
+var possuiFaculdade = false;
+var possuiGraduacao = true;
+
+if(possuiFaculdade) {
+    console.log("Sim, você possui faculdade.");
+    
+} else if(possuiGraduacao) {
+    console.log("Você possui uma Graduação.")
+    
+} else {
+    console.log("Você nao possui faculdade, estude.")
+} 
